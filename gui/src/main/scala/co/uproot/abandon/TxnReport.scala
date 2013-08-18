87f3729e394da57f6e9508d80dd323aec1fc99d4
hrj <harshad.rj@gmail.com>
2013-08-18 17:00:30 +0530

    minor: formatting

--- gui/src/main/scala/co/uproot/abandon/TxnReport.scala
+++ gui/src/main/scala/co/uproot/abandon/TxnReport.scala
@@ -8,19 +8,28 @@ import Helper._
 
 object TxnReport extends Report {
 
-  def mkTxnView(txns:Seq[DetailedTransaction]) = {
+  def mkTxnView(txns: Seq[DetailedTransaction]) = {
     new VBox {
       style = fontStyle
       val maxNameLength = maxElseZero(txns.flatMap(_.parent.get.children.map(_.name.fullPathStr.length)))
       content = txns.map(t => new VBox {
         val grp = t.parent.get
-        val dateLabel = new Label(s"""${t.date.formatYYYYMMDD} ${grp.payeeOpt.getOrElse("")}""") {style="-fx-font-weight:bold"}
-        val grpCommentLabels = grp.groupComments.map(c => new Label("  ;"+c) {style="-fx-font-weight:bold"})
-        val childLabels = grp.children.map(c=>
-          ("  %-"+maxNameLength+"s %20.2f %s") format (c.name, c.delta, c.commentOpt.map("  ; " + _).getOrElse(""))
-        ).map(new Label(_))
+        val grpLabel =
+          new Label(
+            s"""${t.date.formatYYYYMMDD} ${grp.payeeOpt.getOrElse("")}""") {
+            style = "-fx-font-weight:bold"
+          }
+        val grpCommentLabels = grp.groupComments.map(
+          c => new Label("  ;" + c) {
+            style = "-fx-font-weight:bold" 
+          }
+        )
+        val childLabels = grp.children.map{c =>
+          val commentStr = c.commentOpt.map("  ; " + _).getOrElse("")
+          ("  %-" + maxNameLength + "s %20.2f %s") format (c.name, c.delta, commentStr)
+        }.map(new Label(_))
         padding = Insets(10, 10, 10, 10)
-        content = (dateLabel +: grpCommentLabels) ++ childLabels
+        content = (grpLabel +: grpCommentLabels) ++ childLabels
       })
     }
   }
