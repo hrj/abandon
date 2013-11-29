10636079794535d00805f26c8430bc8db01d28b3
hrj <harshad.rj@gmail.com>
2013-11-29 20:40:16 +0530

    Better way to load javafx library into the class path
    Although this is a hack, it seems like a well tested hack (based on
    Stack overflow answers). And it can be removed when Java 8 is released.

--- src/main/scala/co/uproot/abandon/Main.scala
+++ src/main/scala/co/uproot/abandon/Main.scala
@@ -1,9 +1,33 @@
 package co.uproot.abandon
 
+import java.net.{URL, URLClassLoader}
+
 object Main extends App {
+
   if(args.headOption.getOrElse("") equals "-g") {
+    ensureJFXIsAvailable()
     AbandonUI.main(args.tail)
   } else {
     AbandonApp.main(args)
+  }
+
+  def ensureJFXIsAvailable() {
+    try {
+      Class.forName("javafx.event.EventTarget")
+    } catch {
+      case e:java.lang.ClassNotFoundException =>
+        val javaHome = System.getProperty("java.home")
+        addSoftwareLibrary(javaHome, "lib", "jfxrt.jar")
+      case e: java.lang.NoClassDefFoundError =>
+        val javaHome = System.getProperty("java.home")
+        addSoftwareLibrary(javaHome, "lib", "jfxrt.jar")
+    }
+  }
+
+  def addSoftwareLibrary(filePath:String*) = {
+      val file = new java.io.File(filePath.mkString(java.io.File.separator))
+      val method = classOf[URLClassLoader].getDeclaredMethod("addURL", classOf[URL]);
+      method.setAccessible(true);
+      method.invoke(ClassLoader.getSystemClassLoader(), file.toURI.toURL);
   }
 }
