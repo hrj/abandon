2339abc2c3c2ea3b9d078629a3ef511e2c1ad851
sheetalk <sheetal.k1990@gmail.com>
2014-05-17 15:47:58 +0530

    bar chart

--- gui/src/main/scala/co/uproot/abandon/BalanceReport.scala
+++ gui/src/main/scala/co/uproot/abandon/BalanceReport.scala
@@ -6,6 +6,25 @@ import scalafx.scene.control.ListView
 import scalafx.scene.layout.Priority
 import scalafx.scene.input.KeyEvent
 import scalafx.scene.control.ListCell
+import javafx.scene.input.MouseEvent
+import javafx.event.EventHandler
+
+import scalafx.scene.chart.{ AreaChart, NumberAxis, XYChart }
+import scalafx.collections.ObservableBuffer
+import scalafx.scene.chart.Axis
+
+import scalafx.application.JFXApp
+import scalafx.collections.ObservableBuffer
+import scalafx.scene.Scene
+
+//import javafx.scene.{ chart => jfxsc }
+//import scalafx.Includes._
+//import scalafx.application.JFXApp
+//import scalafx.application.JFXApp.PrimaryStage
+//import scalafx.collections.ObservableBuffer
+//import scalafx.scene.Scene
+import scalafx.scene.chart._
+//import scalafx.scene.layout.StackPane
 
 object BalanceReport extends Report {
 
@@ -17,6 +36,52 @@ object BalanceReport extends Report {
 
     class BalanceView(entries: Seq[BalanceReportEntry]) extends ListView(entries) {
       hgrow = Priority.ALWAYS
+      onMouseClicked = new EventHandler[MouseEvent] {
+        override def handle(even: MouseEvent) {
+          val selectedItems = selectionModel().getSelectedItems()
+          selectedItems.collect{
+            case BalanceReportEntry(Some(acc), render) => println(selectedItems); StackedBarChartDemo.aChart
+          }
+        }
+      }
+
+      object StackedBarChartDemo extends JFXApp {
+        def aChart = {
+          val years = Seq("2007", "2008", "2009")
+          val xAxis = CategoryAxis(ObservableBuffer(years))
+          val yAxis = NumberAxis(
+            axisLabel = "Units Sold",
+            lowerBound = 0,
+            upperBound = 8000,
+            tickUnit = 1000
+          )
+
+          stage = new JFXApp.PrimaryStage {
+            title = "Bar Chart"
+            scene = new Scene(500, 500) {
+              root = new BarChart(xAxis, yAxis) {
+                title = "Bar Chart"
+                categoryGap = 25
+                data = ObservableBuffer(
+                  xySeries("Region 1", Seq(567, 1292, 1290)),
+                  xySeries("Region 2", Seq(956, 1665, 2559)),
+                  xySeries("Region 3", Seq(1154, 1927, 2774))
+                )
+              }
+            }
+          }
+
+          /** Create XYChart.Series from a sequence of numbers matching year strings. */
+          def xySeries(name: String, data: Seq[Int]) = {
+            val series = years zip data
+            XYChart.Series[String, Number](
+              name,
+              ObservableBuffer(series.map { case (x, y) => XYChart.Data[String, Number](x, y) })
+            )
+          }
+        }
+      }
+
       onKeyTyped = { e: KeyEvent =>
         // println ("Typed key", e.character, e.code, jfxKeyCode.ENTER, e.delegate.code)
         // if (e.code equals jfxKeyCode.ENTER) {
