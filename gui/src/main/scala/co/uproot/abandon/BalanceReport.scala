544412eaba918d9ebea36c55c83864c75f021268
sheetalk <sheetal.k1990@gmail.com>
2014-05-26 19:19:47 +0530

    GUI - Bar Chart

--- gui/src/main/scala/co/uproot/abandon/BalanceReport.scala
+++ gui/src/main/scala/co/uproot/abandon/BalanceReport.scala
@@ -15,7 +15,7 @@ import scalafx.application.JFXApp
 import scalafx.collections.ObservableBuffer
 import scalafx.scene.Scene
 import scalafx.scene.chart._
-import Helper.{ Zero, maxElseZero, sumDeltas, minElseZero }
+import Helper._
 
 object BalanceReport extends Report {
 
@@ -31,40 +31,133 @@ object BalanceReport extends Report {
         override def handle(even: MouseEvent) {
           val selectedItems = selectionModel().getSelectedItems()
           selectedItems.collect{
-            case BalanceReportEntry(Some(acc), render) => BarChart.aChart(acc)
+            case BalanceReportEntry(Some(acc), render) =>
+              BarChart.aChart(acc)
           }
         }
       }
 
       object BarChart extends JFXApp {
-        def aChart(acc: AccountName) = {
-          val sortedGroup = appState.accState.txnGroups.sortBy(_.date.toInt)
+        def aChart(accName1: AccountName) = {
           var years = Seq[String]()
           var acc1 = Seq[Int]()
+          var accTreeState = Seq[AccountTreeState]()
+          var accName = Seq[AccountName]()
+          var flag2 = 0
+          def isRenderable(accTree: AccountTreeState): Boolean = {
+            if (accTree.countRenderableChildren(accTree => true) != 0) {
+              true
+            } else {
+              false
+            }
+
+          }
+          def trace(accTreeState1: AccountTreeState): Seq[AccountName] = {
+            accTreeState1.childStates.map { c =>
+              if (flag2 == 0) {
+                if (c.name.fullPathStr.matches("""^""" + accName1.fullPathStr + """.*""") == true) {
+                  if (isRenderable(c)) {
+                    if (c.amount.toInt >= 0) {
+                      acc1 :+= c.amount.toInt
+                      accName :+= c.name
+                    }
+                    trace(c)
+                  } else {
+                    acc1 :+= c.total.toInt
+                    accName :+= c.name
+
+                  }
+                } else {
+              if (isRenderable(c)) {
+                trace(c)
+              }
+                }
+              }
+              if (c.name.fullPathStr.equals(accName1.fullPathStr) && isRenderable(c) == false) {
+                acc1 = Seq(c.total.toInt)
+                accName = Seq(c.name)
+                flag2 = 1
+              }
+            }
+            accName
+          }
+          def childCheck(z: AccountTreeState, acc: Seq[AccountName]): Seq[Int] = {
+            accTreeState = Seq(z)
+            if (z.name.fullPathStr.equals(accName1.fullPathStr)) {
+              if (z.amount.toInt != 0) {
+                acc1 :+= z.amount.toInt
+                accName :+= z.name
+                accTreeState = Seq(z)
+              }
+            }
+            accTreeState.map { A =>
+              if (accName1.fullPathStr.matches("""^""" + A.name.fullPathStr + """.*""") == true) {
+                A.childStates.map { c =>
+                  if (c.name.fullPathStr.equals(accName1.fullPathStr) && isRenderable(c) == false) {
+                    acc1 = Seq(c.total.toInt)
+                    accName = Seq(c.name)
+                    flag2 = 1
+                  }
+                  if (flag2 == 0) {
+                    if (isRenderable(c)) { 
+                      if (c.amount.toInt != 0) {
+                        if(c.name.fullPathStr.equals(accName1.fullPathStr) || c.name.fullPathStr.matches("""^""" + accName1.fullPathStr + """.*""")) {
+                        acc1 :+= c.amount.toInt
+                        accName :+= c.name
+                        }
+                      }
+                      trace(c)
+                    } else {
+                    if(c.name.fullPathStr.equals(accName1.fullPathStr) || c.name.fullPathStr.matches("""^""" + accName1.fullPathStr + """.*""")) {
+                      acc1 :+= c.total.toInt
+                      accName :+= c.name
+                    }
+                    }
+                  }
+                }
+              }
+            }
+            acc1
+          }
+          val sortedGroup = appState.accState.txnGroups.sortBy(_.date.toInt)
+          val txnGroups = appState.accState.mkTree(_.exists(c => reportSettings.isAccountMatching(accName1.fullPathStr)))
+          txnGroups.childStates.map { a =>
+            childCheck(a, accName)
+          }
           if (sortedGroup.isEmpty) {
             Nil
           } else {
             val accAmounts = appState.accState.amounts.toSeq
             val maxAmount = maxElseZero(accAmounts.map(_._2.toInt))
             val minAmount = minElseZero(accAmounts.map(_._2.toInt))
+            var flag = 0
             sortedGroup.map { a =>
               a.children.map { b =>
-                if (b.name.equals(acc)) {
-                  acc1 :+= b.delta.toInt
-                  years :+= a.date.formatCompact
+                accName.map { B =>
+                  if (B.fullPathStr.equals(b.name.fullPathStr) && flag == 0) {
+                    years :+= b.date.formatCompact
+                    flag = flag + 1
+                  } else {
+                    if (B.fullPathStr.equals(b.name.fullPathStr) && flag != 0) {
+                      years = (flag + 1).toString +: years
+                      flag = flag + 2
+                    }
+                  }
+
                 }
               }
             }
-            val xAxis = CategoryAxis(ObservableBuffer(years))
+            //   val xAxis = CategoryAxis(ObservableBuffer(years))
+            val xAxis = CategoryAxis(years)
             val yAxis = NumberAxis(
               axisLabel = "Amounts",
-              lowerBound = minAmount,
+              lowerBound = minAmount - 1000,
               upperBound = maxAmount,
-              tickUnit = 20000
+              tickUnit = 1000
             )
             stage = new JFXApp.PrimaryStage {
               title = "Bar Chart"
-              scene = new Scene(500, 500) {
+              scene = new Scene(1200, 1200) {
                 root = new BarChart(xAxis, yAxis) {
                   title = "Bar Chart"
                   categoryGap = 25
