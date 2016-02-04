d9aaeb29d13ac75d3ca4b119c6d42b23a78d1c31
jaa <jaa@sn127.fi>
2016-02-04 22:39:57 +0200

    testrunner-with-args: Support test runner with command line args.
    This adds extra command line arg support for test runner. Also, remove
    old stale output-files, so that they won't mask potential test failures.

--- cli/src/test/scala/co/uproot/abandon/CliTestRunner.scala
+++ cli/src/test/scala/co/uproot/abandon/CliTestRunner.scala
@@ -9,6 +9,8 @@ import org.scalatest.StreamlinedXmlEquality._
 import TestComparator._
 
 import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
 
 class CliTestRunner extends FlatSpec with Matchers with Inside {
 
@@ -23,26 +25,39 @@ class CliTestRunner extends FlatSpec with Matchers with Inside {
 	  val testCases = for (f <- testConfs.flatten) yield {
 	      val testDir = f.toPath.getParent
 	      val basename = f.toPath.getFileName.toString.stripSuffix(".conf")
+
+        val argsPath = Paths.get(testDir.toString, basename + ".args")
+        val args =
+          if (Files.isReadable(argsPath)) {
+            val source = io.Source.fromFile(argsPath.toString)
+            try source.getLines.map(str => str.trim).toArray finally source.close
+          } else {
+            Array[String]()
+          }
+
 	      val refFiles = FileUtils.listFiles(testDir.toString , ".*/" + basename + "\\.ref\\..*")
 	      
 	      val testVecs = for (rf <- refFiles) yield {
 	        val ref = rf.toPath.getFileName.toString()
 	        val output = "out." + basename + "." + ref.stripPrefix(basename + ".ref.")
-	        
-	        if (ref.endsWith(".xml"))
+
+	        // Remove old output files - these can potentially mask test failures
+	        Files.deleteIfExists(Paths.get(testDir.toString, output))
+
+	        if (ref.endsWith(".xml")) {
 	          TestVec(testDir.toString + "/" + output,
 	               testDir.toString + "/" + ref, TestComparator.xmlComparator)
-	        else 
+	        } else {
 	          TestVec(testDir.toString + "/" + output,
-	              testDir.toString + "/" + ref, TestComparator.txtComparator)	        
+	              testDir.toString + "/" + ref, TestComparator.txtComparator)
+	        }
 	      }
-	      TestCase(f.toPath().toString, testVecs.toList)
+	      TestCase(f.toPath().toString, args, testVecs.toList)
 	  }
 	  
 	  for (tc <- testCases) {
-	   
             try {
-	       co.uproot.abandon.CLIMain.runAppThrows(Array("-c", tc.conf))
+	       co.uproot.abandon.CLIMain.runAppThrows(Array("-c", tc.conf) ++ tc.args)
 	    } catch {
                case _: Throwable => assert(false)
             }
