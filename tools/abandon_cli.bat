dca47660442ec700753cbaab1e0a47bd233f97b9
hrj <harshad.rj@gmail.com>
2016-06-24 19:57:28 +0530

    Use JAVA_HOME if set. Closes GH-83

--- tools/abandon_cli.bat
+++ tools/abandon_cli.bat
@@ -1,4 +1,9 @@
 @ECHO OFF
 SET BASEDIR=%~dp0
+SET JAVA_CMD=java
 
-java -jar "%BASEDIR%\start.jar" %*
+if not "%JAVA_HOME%" == "" (
+    SET JAVA_CMD="%JAVA_HOME%\bin\java"
+)
+
+"%JAVA_CMD%" -jar "%BASEDIR%\start.jar" %*
--- tools/abandon_gui.bat
+++ tools/abandon_gui.bat
@@ -1,4 +1,10 @@
 @ECHO OFF
 SET BASEDIR=%~dp0
 
-java -jar "%BASEDIR%\start.jar" -g %*
+SET JAVA_CMD=java
+
+if not "%JAVA_HOME%" == "" (
+    SET JAVA_CMD="%JAVA_HOME%\bin\java"
+)
+
+"%JAVA_CMD%" -jar "%BASEDIR%\start.jar" -g %*
