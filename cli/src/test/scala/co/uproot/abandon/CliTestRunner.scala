68a80d2791d0a08096a2602a3945c1ee31300f99
hrj <harshad.rj@gmail.com>
2016-09-25 20:49:23 +0530

    internal: register each cli test individually

--- cli/src/test/scala/co/uproot/abandon/CliTestRunner.scala
+++ cli/src/test/scala/co/uproot/abandon/CliTestRunner.scala
@@ -2,54 +2,58 @@ package co.uproot.abandon
 
 import java.nio.file.{Files, Paths}
 
-import org.scalatest.{FlatSpec, Inside, Matchers}
+import org.scalatest.{FunSuite}
 
-class CliTestRunner extends FlatSpec with Matchers with Inside {
+class CliTestSuite extends FunSuite {
 
-  "Testrunner" should "automatically test multiple dirs and confs" in {
-
-    val testConfs =
-      for (d <- FileUtils.listDirs("../testCases", ".*/sclT[0-9]+(-.*)$")) yield {
-        for (f <- FileUtils.listFilesRecursive(d.getAbsolutePath, ".*\\.conf$")) yield { f }
-      }
-
-    val testCases = for (f <- testConfs.flatten) yield {
-      val testDir = f.toPath.getParent
-      val basename = f.toPath.getFileName.toString.stripSuffix(".conf")
-
-      val argsPath = Paths.get(testDir.toString, basename + ".args")
-      val args =
-        if (Files.isReadable(argsPath)) {
-          val source = io.Source.fromFile(argsPath.toString)
-          try source.getLines.map(str => str.trim).toArray finally source.close
-        } else {
-          Array[String]()
-        }
-
-      val refFiles = FileUtils.listFiles(testDir.toString, ".*/" + basename + "\\.ref\\..*")
-
-      val testVecs = for (rf <- refFiles) yield {
-        val ref = rf.toPath.getFileName.toString()
-        val output = "out." + basename + "." + ref.stripPrefix(basename + ".ref.")
-
-        /*
-         * Old output files - these can potentially mask test failures
-         * Let's err on safe side and not delete files for now.
-         * TODO: Use conditionally in memory file system for output
-         */
-
-        if (ref.endsWith(".xml")) {
-          TestVec(testDir.toString + "/" + output,
-            testDir.toString + "/" + ref, TestComparator.xmlComparator)
-        } else {
-          TestVec(testDir.toString + "/" + output,
-            testDir.toString + "/" + ref, TestComparator.txtComparator)
-        }
-      }
-      TestCase(f.toPath().toString, args, testVecs.toList)
+  val testConfs =
+    for (d <- FileUtils.listDirs("../testCases", ".*/sclT[0-9]+(-.*)$")) yield {
+      for (f <- FileUtils.listFilesRecursive(d.getAbsolutePath, ".*\\.conf$")) yield { f }
     }
 
-    for (tc <- testCases) {
+  val testCases = for (f <- testConfs.flatten) yield {
+    val testDir = f.toPath.getParent
+    val basename = f.toPath.getFileName.toString.stripSuffix(".conf")
+
+    val argsPath = Paths.get(testDir.toString, basename + ".args")
+    val args =
+      if (Files.isReadable(argsPath)) {
+        val source = io.Source.fromFile(argsPath.toString)
+        try source.getLines.map(str => str.trim).toArray finally source.close
+      } else {
+        Array[String]()
+      }
+
+    val refFiles = FileUtils.listFiles(testDir.toString, ".*/" + basename + "\\.ref\\..*")
+
+    val testVecs = for (rf <- refFiles) yield {
+      val ref = rf.toPath.getFileName.toString()
+      val output = "out." + basename + "." + ref.stripPrefix(basename + ".ref.")
+
+      /*
+       * Old output files - these can potentially mask test failures
+       * Let's err on safe side and not delete files for now.
+       * TODO: Use conditionally in memory file system for output
+       */
+
+      if (ref.endsWith(".xml")) {
+        TestVec(testDir.toString + "/" + output,
+          testDir.toString + "/" + ref, TestComparator.xmlComparator)
+      } else {
+        TestVec(testDir.toString + "/" + output,
+          testDir.toString + "/" + ref, TestComparator.txtComparator)
+      }
+    }
+    TestCase(f.toPath().toString, args, testVecs.toList)
+  }
+
+  for (tc <- testCases) {
+    registerTest(tc.conf) {
+      runTest(tc)
+    }
+  }
+
+  def runTest(tc: TestCase) {
       try {
         co.uproot.abandon.CLIMain.runAppThrows(Array("-c", tc.conf) ++ tc.args)
       } catch {
@@ -58,6 +62,5 @@ class CliTestRunner extends FlatSpec with Matchers with Inside {
       for (testfiles <- tc.testVec) {
         assert(testfiles.comparator(testfiles.output, testfiles.reference), "Failed comparison for " + testfiles)
       }
-    }
   }
 }
