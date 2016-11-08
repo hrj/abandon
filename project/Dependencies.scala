f20d4dedeed6f1de27abc3b59c86a4866918ac85
hrj <harshad.rj@gmail.com>
2016-11-08 13:06:14 +0530

    updated dependency: scallop

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ import Keys._
 object Dependencies {
   // Versions
   val scalatestVersion = "3.0.0"
-  val scallopVersion = "2.0.2"
+  val scallopVersion = "2.0.5"
   val scalaMockVersion = "3.3.0"
   val configVersion = "1.3.1"
   val scalaXMLVersion = "1.0.6"
