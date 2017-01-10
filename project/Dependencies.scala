30f243177549252072c50ff54a88f015aa5a9978
hrj <harshad.rj@gmail.com>
2017-01-10 17:16:33 +0530

    updated dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,12 +3,12 @@ import Keys._
 
 object Dependencies {
   // Versions
-  val scalatestVersion = "3.0.0"
-  val scallopVersion = "2.0.5"
-  val scalaMockVersion = "3.3.0"
+  val scalatestVersion = "3.0.1"
+  val scallopVersion = "2.0.6"
+  val scalaMockVersion = "3.4.2"
   val configVersion = "1.3.1"
   val scalaXMLVersion = "1.0.6"
-  val scalaParserCombinatorsVersion = "1.0.4"
+  val scalaParserCombinatorsVersion = "1.0.5"
   val scalaFXVersion = "8.0.102-R11"
 
   // Libraries
