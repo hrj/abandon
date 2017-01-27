b73f62be285a13f4a9ffc05e81cb04ad353a8a22
Gerd Reiss <gerd.reiss@loewenfels.ch>
2017-01-27 09:29:40 +0100

    Adding more tests

--- cli/src/test/scala/co/uproot/abandon/CLIMainTest.scala
+++ cli/src/test/scala/co/uproot/abandon/CLIMainTest.scala
@@ -8,9 +8,19 @@ class CLIMainTest extends FlatSpec with Matchers {
   it should "handle version" in {
     co.uproot.abandon.CLIApp.run(Array("--version"))
   }
-  it should "handle -h by throwing the Help exception" in {
+  it should "handle -h by throwing a Help exception" in {
     an [org.rogach.scallop.exceptions.Help] should be thrownBy {
       co.uproot.abandon.CLIApp.run(Array("-h"))
+    }
+  }
+  it should "handle --help by throwing a Help exception" in {
+    an [org.rogach.scallop.exceptions.Help] should be thrownBy {
+      co.uproot.abandon.CLIApp.run(Array("--help"))
+    }
+  }
+  it should "handle an unknown option by throwing an UnknownOption exception" in {
+    an [org.rogach.scallop.exceptions.UnknownOption] should be thrownBy {
+      co.uproot.abandon.CLIApp.run(Array("--unknown"))
     }
   }
 }
