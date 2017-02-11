2b16181c842c6fd4453d84911e4732585ed08880
Gerd Reiss <gerdreiss@jscriptive.com>
2017-02-11 18:11:37 +0100

    Update transactions view when reports are updated

--- gui/src/main/scala/co/uproot/abandon/RegReport.scala
+++ gui/src/main/scala/co/uproot/abandon/RegReport.scala
@@ -1,19 +1,25 @@
 package co.uproot.abandon
 
 import scalafx.Includes._
-import scalafx.scene.control.TreeView
-import scalafx.scene.control.TreeItem
-import scalafx.scene.input.KeyEvent
-import scalafx.scene.control.TreeCell
-import scalafx.stage.Stage
+import scalafx.application.Platform
 import scalafx.scene.Scene
-import scalafx.scene.control.ScrollPane
-import scalafx.stage.Modality
+import scalafx.scene.control.{ScrollPane, TreeCell, TreeItem, TreeView}
+import scalafx.scene.input.KeyEvent
+import scalafx.stage.{Modality, Stage}
 
 object RegUIReport extends UIReport {
   private def getNestedTxns(item: TreeItem[RegisterReportEntry]): Seq[DetailedPost] = {
     item.getValue.txns ++ item.children.flatMap(getNestedTxns(_))
   }
+  private def getTxContent(selectedItem: TreeItem[RegisterReportEntry]) = {
+    new ScrollPane {
+      content = TxnUIReport.mkTxnView(getNestedTxns(selectedItem))
+    }
+  }
+
+  var selectedItem: TreeItem[RegisterReportEntry] = null
+  var txStage: Stage = null
+  var trxShown: Boolean = false
 
   def mkRegisterReport(appState: AppState, reportSettings: RegisterReportSettings) = {
     val registers = Reports.registerReport(appState, reportSettings)
@@ -30,34 +36,46 @@ object RegUIReport extends UIReport {
     new TreeView(reportRoot) {
       styleClass += styleClassName
       onKeyTyped = { e: KeyEvent =>
-        if (e.character equals "\r") {
-          val selectedItemOpt = selectionModel().getSelectedItems().headOption
-          selectedItemOpt foreach { selectedItem =>
-            // println(e)
-            val txStage = new Stage() {
-              scene = new Scene(800, 600) {
-                root = new ScrollPane {
-                  content = TxnUIReport.mkTxnView(getNestedTxns(selectedItem))
-                }
-                stylesheets += "default_theme.css"
-              }
-              initModality(Modality.ApplicationModal)
-              title = "Transactions"
+        if (e.character.equals("\r") && !selectionModel().isEmpty) {
+          // println(e)
+          selectedItem = selectionModel().getSelectedItem
+          txStage = new Stage() {
+            scene = new Scene(800, 600) {
+              root = getTxContent(selectedItem)
+              stylesheets += "default_theme.css"
             }
-            txStage.show
+            initModality(Modality.ApplicationModal)
+            title = "Transactions"
           }
+          txStage.onCloseRequest = () => trxShown = false
+          txStage.show
+          trxShown = true
         }
       }
-      cellFactory = { v =>
+      cellFactory = { _ =>
         val delegate = new javafx.scene.control.TreeCell[RegisterReportEntry]() {
+          private def refreshTrxs(t: RegisterReportEntry) = {
+            if (trxShown && selectedItem.getValue == t) {
+              selectedItem =
+                if (selectedItem.getValue.render == reportRoot.getValue.render) reportRoot
+                else registerItems.find(item => item.getValue == t).getOrElse(selectedItem)
+              Platform.runLater(() => {
+                txStage.delegate.scene().root() = getTxContent(selectedItem)
+              })
+            }
+          }
           override def updateItem(t: RegisterReportEntry, empty: Boolean) = {
             super.updateItem(t, empty)
-            setText(if (t != null) t.render else null)
+            if (t == null) {
+              setText(null)
+            } else {
+              setText(t.render)
+              refreshTrxs(t)
+            }
           }
         }
         new TreeCell(delegate)
       }
     }
   }
-
 }
