6056ba9ad4ca1236f924df8d0949ab7e31fb1a88
Gerd Reiss <gerdreiss@jscriptive.com>
2017-02-14 20:09:59 +0100

    Update transactions view when reports are updated, take 2

--- gui/src/main/scala/co/uproot/abandon/RegReport.scala
+++ gui/src/main/scala/co/uproot/abandon/RegReport.scala
@@ -1,5 +1,6 @@
 package co.uproot.abandon
 
+import scala.util.matching.Regex
 import scalafx.Includes._
 import scalafx.application.Platform
 import scalafx.scene.Scene
@@ -8,28 +9,56 @@ import scalafx.scene.input.KeyEvent
 import scalafx.stage.{Modality, Stage}
 
 object RegUIReport extends UIReport {
-  private def getNestedTxns(item: TreeItem[RegisterReportEntry]): Seq[DetailedPost] = {
-    item.getValue.txns ++ item.children.flatMap(getNestedTxns(_))
+
+  case class TxnItem(txnGroup: String, reportEntry: RegisterReportEntry)
+
+  implicit class TxnItemMatcher(t: TxnItem) {
+    val accountMatcher: Regex = """[^\s]+""".r
+    def matches(item: TreeItem[TxnItem]): Boolean = {
+      isMatchingParent(item) || isMatchingTxnGroup(item)
+    }
+    private def isMatchingParent(item: TreeItem[TxnItem]) = {
+      t.reportEntry.txns.isEmpty && t.reportEntry.render == item.getValue.reportEntry.render
+    }
+    private def isMatchingTxnGroup(item: TreeItem[TxnItem]) = {
+      t.reportEntry.txns.nonEmpty && t.txnGroup == item.getValue.txnGroup &&
+        accountName(t.reportEntry) == accountName(item.getValue.reportEntry)
+    }
+    private def accountName(entry: RegisterReportEntry): String = {
+      accountMatcher.findFirstIn(t.reportEntry.render).getOrElse(t.reportEntry.render)
+    }
   }
-  private def getTxContent(selectedItem: TreeItem[RegisterReportEntry]) = {
+
+  private def getNestedTxns(item: TreeItem[TxnItem]): Seq[DetailedPost] = {
+    filterSelectedDates(item.getValue.reportEntry.txns) ++ item.children.flatMap(getNestedTxns(_))
+  }
+  def filterSelectedDates(txns: Seq[DetailedPost]): Seq[DetailedPost] = {
+    if (selectedTxnDates.isEmpty) txns
+    else txns.filter(txn => selectedTxnDates.contains(txn.date))
+  }
+  private def getSelectedTxnDates(item: TreeItem[TxnItem]): Seq[Date] = {
+    item.getValue.reportEntry.txns.groupBy(_.date).unzip._1.toSeq ++ item.children.flatMap(getSelectedTxnDates(_))
+  }
+  private def getTxContent(selectedItem: TreeItem[TxnItem]) = {
     new ScrollPane {
       content = TxnUIReport.mkTxnView(getNestedTxns(selectedItem))
     }
   }
 
-  var selectedItem: TreeItem[RegisterReportEntry] = _
+  var selectedItem: TreeItem[TxnItem] = _
+  var selectedTxnDates: Seq[Date] = Nil
   var txStage: Stage = _
-  var trxShown: Boolean = false
 
   def mkRegisterReport(appState: AppState, reportSettings: RegisterReportSettings) = {
     val registers = Reports.registerReport(appState, reportSettings)
     val registerItems = registers.map { rg =>
-      new TreeItem(RegisterReportEntry(Nil, rg.groupTitle)) {
-        children = rg.entries.map(new TreeItem(_))
+      new TreeItem(TxnItem(rg.groupTitle, RegisterReportEntry(Nil, rg.groupTitle))) {
+        children = rg.entries.map(entry => new TreeItem(TxnItem(rg.groupTitle, entry)))
         expanded = true
       }
     }
-    val reportRoot = new TreeItem(RegisterReportEntry(Nil, "Register report (account, delta, total)")) {
+    val rootTitle = "Register report (account, delta, total)"
+    val reportRoot = new TreeItem(TxnItem(rootTitle, RegisterReportEntry(Nil, rootTitle))) {
       children = registerItems
       expanded = true
     }
@@ -39,9 +68,7 @@ object RegUIReport extends UIReport {
         if (e.character.equals("\r") && !selectionModel().isEmpty) {
           // println(e)
           selectedItem = selectionModel().getSelectedItem
-          // show all transactions of the parent of the selected item if the latter has any transactions
-          // this will allow for an update of the transaction view when an input file changes
-          if (selectedItem.getValue.txns.nonEmpty) selectedItem = selectedItem.getParent
+          selectedTxnDates = getSelectedTxnDates(selectedItem)
           txStage = new Stage() {
             scene = new Scene(800, 600) {
               root = getTxContent(selectedItem)
@@ -50,34 +77,33 @@ object RegUIReport extends UIReport {
             initModality(Modality.ApplicationModal)
             title = "Transactions"
           }
-          txStage.onCloseRequest = () => trxShown = false
           txStage.show
-          trxShown = true
         }
       }
       cellFactory = { _ =>
-        val delegate = new javafx.scene.control.TreeCell[RegisterReportEntry]() {
-          private def refreshTrxs(t: RegisterReportEntry) = {
-            if (trxShown && selectedItem.getValue == t) {
-              selectedItem =
-                if (selectedItem.getValue.render == reportRoot.getValue.render) reportRoot
-                else registerItems.find(item => item.getValue == t).getOrElse(selectedItem)
-              Platform.runLater(() => {
-                txStage.delegate.scene().root() = getTxContent(selectedItem)
-              })
-            }
-          }
-          override def updateItem(t: RegisterReportEntry, empty: Boolean): Unit = {
+        val delegate = new javafx.scene.control.TreeCell[TxnItem]() {
+          override def updateItem(t: TxnItem, empty: Boolean): Unit = {
             super.updateItem(t, empty)
             if (t == null) {
               setText(null)
             } else {
-              setText(t.render)
+              setText(t.reportEntry.render)
               refreshTrxs(t)
             }
           }
         }
         new TreeCell(delegate)
+      }
+      private def refreshTrxs(t: TxnItem) = {
+        if (txStage != null && txStage.showing() && (t matches selectedItem)) {
+          // update the selected item
+          selectedItem =
+            if (t matches reportRoot) reportRoot
+            else registerItems.find(t matches _).getOrElse(selectedItem)
+          Platform.runLater(() => {
+            txStage.delegate.scene().root() = getTxContent(selectedItem)
+          })
+        }
       }
     }
   }
