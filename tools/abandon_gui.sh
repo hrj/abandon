63627b3ca5fd53e672addcbf4497f904898b7959
Colin Dean <colindean@users.noreply.github.com>
2017-07-03 13:12:32 -0400

    Use ledger.png
    This way, it retains the original name

--- tools/abandon_gui.sh
+++ tools/abandon_gui.sh
@@ -11,7 +11,7 @@ EXTRA_OPTIONS=""
 if [ "$(uname -s)" == "Darwin" ]
 then
   # The dock name won't actually work until https://bugs.openjdk.java.net/browse/JDK-8029440 is fixed
-  EXTRA_OPTIONS='-Xdock:icon=abandon.png -Xdock:name=Abandon -Dcom.apple.mrj.application.apple.menu.about.name=Abandon -Dapple.awt.application.name=Abandon'
+  EXTRA_OPTIONS='-Xdock:icon=ledger.png -Xdock:name=Abandon -Dcom.apple.mrj.application.apple.menu.about.name=Abandon -Dapple.awt.application.name=Abandon'
 fi
 
 $JAVA_CMD ${EXTRA_OPTIONS} -jar ${BASEDIR}/lib/abandon.jar -g $*
