7fa8773193971b9ce4f4d91293f82c1ac6a356b8
Colin Dean <colindean@users.noreply.github.com>
2017-07-04 02:29:09 -0400

    Adds OSX dock options (#151)
    * Adds OSX dock options to GUI launcher
    
    * Use ledger.png
    
    This way, it retains the original name

--- tools/abandon_gui.sh
+++ tools/abandon_gui.sh
@@ -7,4 +7,11 @@ fi
 
 BASEDIR=`dirname $0`
 
-$JAVA_CMD -jar ${BASEDIR}/lib/abandon.jar -g $*
+EXTRA_OPTIONS=""
+if [ "$(uname -s)" == "Darwin" ]
+then
+  # The dock name won't actually work until https://bugs.openjdk.java.net/browse/JDK-8029440 is fixed
+  EXTRA_OPTIONS='-Xdock:icon=ledger.png -Xdock:name=Abandon -Dcom.apple.mrj.application.apple.menu.about.name=Abandon -Dapple.awt.application.name=Abandon'
+fi
+
+$JAVA_CMD ${EXTRA_OPTIONS} -jar ${BASEDIR}/lib/abandon.jar -g $*
