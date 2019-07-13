e63c6e3fd802c3b8a81d0f83f416e7229ccb0938
Harshad RJ <harshad.rj@gmail.com>
2019-07-13 12:51:39 +0530

    updated deps

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,15 +10,15 @@ object Dependencies {
   val scalaXMLVersion = "1.2.0"
   val scalaParserCombinatorsVersion = "1.1.2"
   val scalaFXVersion = "8.0.192-R14"
-  val dirSuiteVersion = "0.6.0"
+  val dirSuiteVersion = "0.30.1"
 
   // Libraries
   val scalatest = "org.scalatest" %% "scalatest" % scalatestVersion
-  val scalamock = "org.scalamock" %% "scalamock-scalatest-support" % scalaMockVersion
+  val scalamock = "org.scalamock" %% "scalamock" % scalaMockVersion
   val scallop = "org.rogach" %% "scallop" % scallopVersion
   val config = "com.typesafe" % "config" % configVersion
   val scalaXML = "org.scala-lang.modules" %% "scala-xml" % scalaXMLVersion
   val scalaParserCombinators = "org.scala-lang.modules" %% "scala-parser-combinators" % scalaParserCombinatorsVersion
   val scalaFX = "org.scalafx" %% "scalafx" % scalaFXVersion
-  val dirSuite =  "fi.sn127" %% "utils-testing" % dirSuiteVersion
+  val dirSuite = "fi.e257.testing" %% "dirsuite" % dirSuiteVersion
 }
