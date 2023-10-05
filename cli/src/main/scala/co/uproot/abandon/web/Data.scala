7280ac03d8264dc31aaa38c68ace459b102b1c89
hrj <hrj@users.noreply.github.com>
2023-10-05 17:44:55 +0530

    web-api first cut

---
+++ cli/src/main/scala/co/uproot/abandon/web/Data.scala
@@ -0,0 +1,122 @@
+package co.uproot.abandon.web
+
+import co.uproot.abandon.Date
+import co.uproot.abandon.web.Util.{except, sumDeltas, Zero}
+
+trait WithDelta {
+  val delta: BigDecimal
+}
+
+/** A transaction as parsed from input */
+case class Transaction(
+                        date: Date,
+                        annotationOpt: Option[String],
+                        payeeOpt: Option[String],
+                        comments: Seq[String],
+                        posts: Seq[Post]) {
+
+  def splitPostsByCount = {
+    val (posPosts, negPosts) = posts.partition(_.delta > Zero)
+    if (posPosts.length > negPosts.length) {
+      (posPosts, negPosts)
+    } else {
+      (negPosts, posPosts)
+    }
+  }
+
+  def demuxPosts = {
+    val (majorityPosts, minorityPosts) = splitPostsByCount
+    val minorityAccountNames = minorityPosts.map(_.name).toSet
+    // assert(minorityAccountNames.size == 1, "Currently, minority Post can only be from one account. MinorityAccountNames: " + minorityAccountNames.mkString(", "))
+    if (majorityPosts.length == minorityPosts.length || minorityAccountNames.size != 1) {
+      // Demux not required
+      this
+    } else {
+      // Demux possible
+      assert(minorityPosts.length == 1, "Currently, only one minority Post supported")
+      val minorTxn = minorityPosts.head
+      val demuxedPosts = majorityPosts.map(major => Post(minorTxn.name, -major.delta, minorTxn.commentOpt))
+      copy(posts = majorityPosts ++ demuxedPosts)
+    }
+  }
+
+  def cookPosts: Seq[CookedPost] = {
+    val (majorityPosts, minorityPosts) = splitPostsByCount
+    val (majorityCount, minorityCount) = (majorityPosts.length, minorityPosts.length)
+    if (majorityCount == 1 && minorityCount == 1) {
+      // Simple case
+      posts.map { _.simpleCook }
+    } else {
+      val majorityAccountNames = majorityPosts.map(_.name).toSet
+      if (minorityCount == 1 && majorityAccountNames.size == 1) {
+        // Needs simple demux
+        val minorityCookedTxn = minorityPosts.head.simpleCook
+        val majorityAccountName = majorityAccountNames.head
+        val oppOtherPosts = majorityPosts.map { mt =>
+          CookedOtherPost(minorityCookedTxn.name, -mt.delta, mt.splitComments)
+        }
+        val majorityCookedTxn = CookedPost(date, majorityAccountName, sumDeltas(majorityPosts), comments.toSeq, oppOtherPosts, Nil)
+        Seq(majorityCookedTxn, minorityCookedTxn)
+      } else {
+        posts.map { _.simpleCook }
+      }
+    }
+  }
+
+  def updateTxnRefs(): Unit = {
+    posts.zipWithIndex.foreach {
+      case (post, idx) =>
+        post.other = except(posts, idx)
+        post.txn = this
+    }
+  }
+}
+
+/** A post as parsed from input */
+case class Post(
+                 name: String,
+                 delta: BigDecimal,
+                 commentOpt: Option[String]) extends WithDelta {
+
+  var other: Seq[Post] = Nil
+  var txn: Transaction = null
+
+  val isPositive = delta > Zero
+
+  def getBalancedOther = {
+    other.partition(isPositive == _.isPositive)
+  }
+
+  val splitComments:Seq[String] = commentOpt.map(_.split(";").toList).getOrElse(Nil)
+
+  // TODO: All annotations should not be prefixed by "Chq #". Should be a config setting either here or in abandon
+  def comments = txn.annotationOpt.map("Chq #" + _) ++ txn.payeeOpt ++ splitComments ++ txn.comments
+
+  def date = txn.date
+
+  def simpleCook = {
+    val (similarOther, oppOther) = getBalancedOther
+    val oppOtherCooked = oppOther.map(o => CookedOtherPost(o.name, o.delta, o.splitComments))
+    val similarOtherCooked = similarOther.map(o => CookedOtherPost(o.name, o.delta, o.splitComments))
+    CookedPost(date, name, delta, comments.toSeq, oppOtherCooked, similarOtherCooked)
+  }
+}
+
+/** A transaction that is ready to be reported as the other entry of a transaction.
+ * All the analysis has to be done previously */
+case class CookedOtherPost(name: String, delta: BigDecimal, comments: Seq[String]) extends WithDelta {
+  override def toString = {
+    name + ": " + delta
+  }
+}
+
+/** A post that is ready to be reported.
+ * All the analysis has to be done previously */
+case class CookedPost(date: Date, name: String, delta: BigDecimal, comments: Seq[String], oppositeOthers: Seq[CookedOtherPost], similarOthers: Seq[CookedOtherPost]) extends WithDelta {
+  val isComplex = math.min(oppositeOthers.length, similarOthers.length + 1) > 1
+  override def toString = {
+    "" + date + " " + name + ": " + delta + "\n  Opposites: " + oppositeOthers + "\n  similars: " + similarOthers
+  }
+}
+
+
---
+++ cli/src/main/scala/co/uproot/abandon/web/WebAPI.scala
@@ -0,0 +1,208 @@
+package co.uproot.abandon.web
+
+import co.uproot.abandon.{AccountName, AccountTreeState, AppState, Date, JournalExportSettings}
+
+import java.io.FileWriter
+
+object Util {
+  val Zero = BigDecimal(0)
+
+  def sumDeltas(s: Seq[WithDelta]) = s.foldLeft(Zero)(_ + _.delta)
+  def except[T](a: Seq[T], i: Int) = a.slice(0, i) ++ a.slice(i + 1, a.length)
+}
+
+import  Util._
+
+case class AccountDetails(name: AccountName, openingBalance: BigDecimal, closingBalance: BigDecimal, debitSubTotal: BigDecimal, creditSubTotal: BigDecimal, posts: Seq[CookedPost])
+
+case class AccountSemiDetails(name: String, debitSubTotal: Option[BigDecimal], creditSubTotal: Option[BigDecimal])
+
+object WebAPI {
+  def execute(jes: JournalExportSettings, startDate: Date, appState: AppState, filterDescription: Option[String]): Unit = {
+    // TODO: show filterDescription in output
+    val accState = appState.accState
+    // TODO: Apply filter from settings, if not already applied
+    val accTree = accState.mkTree(_ => true)
+
+    val txns = accState.postGroups.map(pg => {
+      val txnPosts = pg.children.map(cp => {
+        Post(cp.name.fullPathStr, cp.delta, cp.commentOpt)
+      })
+      val txn = Transaction(pg.date, pg.annotationOpt, pg.payeeOpt, pg.groupComments, txnPosts)
+      txn.updateTxnRefs()
+      txn
+    })
+    val cookedPosts = txns.flatMap(_.cookPosts)
+
+    execute(startDate, cookedPosts, accTree)
+  }
+
+  private def execute(startDate: Date, posts: Seq[CookedPost], accTree: AccountTreeState): Unit = {
+    val (currPosts, prevPosts) = posts.partition(_.date.toInt >= startDate.toInt)
+    val allAccountNames = posts.map(_.name).toSet
+    val sortedAllAccountNames = allAccountNames.toSeq.sorted
+    val groupedPosts = currPosts.groupBy(_.name)
+    val openingBalances = prevPosts.groupBy(_.name).map { case (name, gPosts) => (name -> sumDeltas(gPosts)) }
+    val closingBalances = sortedAllAccountNames.map(name => {
+      val openingBalance = openingBalances.getOrElse(name, Zero)
+      val gposts = groupedPosts.getOrElse(name, Nil)
+      val closing = openingBalance + sumDeltas(gposts)
+      name -> closing
+    }).toMap
+
+    def getDetails(name: String, filterPost: CookedPost => Boolean = _ => true) = {
+      val gPosts = groupedPosts.getOrElse(name, Nil).filter(filterPost)
+      val (debitPosts, creditPosts) = gPosts.partition(_.delta < Zero)
+      val openingBalance = openingBalances.getOrElse(name, Zero)
+      val closingBalance = closingBalances.getOrElse(name, Zero)
+      val debitSubTotal = sumDeltas(debitPosts)
+      val creditSubTotal = sumDeltas(creditPosts)
+      val fullPath = name.split(':')
+      AccountDetails(AccountName(fullPath), openingBalance, closingBalance, debitSubTotal, creditSubTotal, gPosts)
+    }
+
+    def mkBalanceReport(node: AccountTreeState): Map[String, Any] = {
+      val name = node.name
+      val details = getDetails(name.fullPathStr)
+      Map(
+        "name" -> name.name,
+        "opening" -> details.openingBalance,
+        "closing" -> details.closingBalance,
+        "debit" -> details.debitSubTotal,
+        "credit" -> details.creditSubTotal,
+        "children" -> node.childStates.map(mkBalanceReport).toArray,
+      )
+    }
+
+    var accTxnsReport = Map[String, Any]()
+
+    def getDetailsByMonth(accName: AccountName): Map[String, Any] = {
+      val name = accName.fullPathStr
+      val gPosts = currPosts.filter(_.name == name)
+      val openingBalance = openingBalances.getOrElse(name, Zero)
+      val closingBalance = openingBalance + sumDeltas(gPosts)
+      val months = gPosts.map(p => Date(p.date.year, p.date.month, 1)).toSet.toArray
+      var allDetails = Map[String, Any]()
+      var prevMonthClosingBalance = openingBalance
+
+      def txnsToMap(posts: Seq[CookedPost], openingBalance: BigDecimal) = {
+        var currBalance = openingBalance
+        posts.map(p =>
+          val balanceAfterPost = currBalance + p.delta
+          currBalance = balanceAfterPost
+          Map(
+            "date" -> p.date.formatYYYYMMDD,
+            "indeterminate" -> p.isComplex,
+            "selfPost" -> Map(
+              "debit" -> (p.delta < Zero),
+              "delta" -> p.delta,
+              "balance" -> balanceAfterPost
+            ),
+            "posts" -> (p.oppositeOthers ++ p.similarOthers).map(o =>
+              Map(
+                "debit" -> (o.delta < Zero),
+                "delta" -> o.delta,
+                "name" -> o.name
+              )
+            ).toArray
+          )
+        ).toArray
+      }
+
+      def getDetailsForMonth(mDate: Date): Map[String, Any] = {
+        val rawDetails = getDetails(name, p => p.date.year == mDate.year && p.date.month == mDate.month)
+        val monthClosingBalance = prevMonthClosingBalance + rawDetails.debitSubTotal + rawDetails.creditSubTotal
+        val monthOpeningBalance = prevMonthClosingBalance
+        val debits = rawDetails.debitSubTotal
+        val credits = rawDetails.creditSubTotal
+        prevMonthClosingBalance = monthClosingBalance
+
+        Map(
+          "opening" -> monthOpeningBalance,
+          "closing" -> monthClosingBalance,
+          "debit" -> debits,
+          "credit" -> credits,
+          "txns" -> txnsToMap(rawDetails.posts, monthOpeningBalance)
+        )
+      }
+
+      months.sortBy(_.toInt).foreach(mDate =>
+        allDetails += s"${mDate.year} / ${mDate.month}" -> getDetailsForMonth(mDate)
+      )
+      assert(prevMonthClosingBalance == closingBalance, s"prev monthly closing bal = $prevMonthClosingBalance, acc closing: $closingBalance")
+      allDetails
+    }
+
+    def updateAccTxnReport(node: AccountTreeState): Unit = {
+      val name = node.name.fullPathStr
+      if (name.nonEmpty) {
+        val txnsByMonth = getDetailsByMonth(node.name)
+        accTxnsReport ++= Map(
+          name -> Map(
+            "name" -> name,
+            "txnsByMonth" -> txnsByMonth
+          )
+        )
+      }
+      node.childStates.foreach(updateAccTxnReport)
+    }
+
+    val balReport = if (accTree.name.fullPathStr.isEmpty) accTree.childStates.map(mkBalanceReport).toArray else mkBalanceReport(accTree)
+
+    updateAccTxnReport(accTree)
+
+    val fullReport = Map(
+      "txns" -> Array[String](),
+      "accountBalances" -> balReport,
+      "accountTxns" -> accTxnsReport
+    )
+
+    val writer = FileWriter("web.json")
+    writer.write(serializeJSON(fullReport))
+    writer.close()
+
+  }
+
+  private def serializeJSON(any: Any): Array[Char] = {
+    any match {
+      case map: Map[String, Any] =>
+        val sb = StringBuilder(1024)
+        val keys = map.keys.toList
+        val maxIndex = keys.size - 1
+        sb.append('{')
+        keys.zipWithIndex.foreach(ki => {
+          val key = ki._1
+          val index = ki._2
+          val value = map(key)
+          sb.append('"')
+          sb.append(key)
+          sb.append('"')
+          sb.append(':')
+          sb.appendAll(serializeJSON(value))
+          if (index != maxIndex) {
+            sb.append(',')
+          }
+        })
+        sb.append('}')
+        sb.toCharArray
+      case array: Array[Any] =>
+        val sb = StringBuilder(1024)
+        val maxIndex = array.size - 1
+        // test
+        sb.append('[')
+        array.zipWithIndex.foreach(vi => {
+          val v = vi._1
+          val index = vi._2
+          sb.appendAll(serializeJSON(v))
+          if (index != maxIndex) {
+            sb.append(',')
+          }
+        })
+        sb.append(']')
+        sb.toCharArray
+      case b: Boolean => ("" + b).toCharArray
+      case s: String => ('"' + s + '"').toCharArray
+      case any: Any => ('"' + any.toString + '"').toCharArray
+    }
+  }
+}
