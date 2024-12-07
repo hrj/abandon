3307d4205caadd8f60950de9167f822257cea88d
Scala Steward <scala_steward@virtuslab.com>
2024-12-07 15:03:17 +0000

    Update scallop to 5.2.0
    Signed-off-by: Scala Steward <scala_steward@virtuslab.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ import Keys._
 object Dependencies {
   // Versions
   val scalatestVersion = "3.2.19"
-  val scallopVersion = "5.1.0"
+  val scallopVersion = "5.2.0"
   val configVersion = "1.4.3"
   val scalaXMLVersion = "2.3.0"
   val scalaParserCombinatorsVersion = "2.4.0"
